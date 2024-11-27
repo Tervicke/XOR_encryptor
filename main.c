65ba0d67a16315965d535ce8f3b7c5178ef3363a
tervicke <ameywpatil@gmail.com>
2024-11-27 19:13:38 +0530

    added utils.c file for to seperate help menu function from the main file

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+void help_menu();
+void encypt();
+void decrypt();
+
 typedef struct{
 	char *input_file; // mandatory
 	char *key_file; // 
@@ -13,32 +18,6 @@ typedef struct{
 	char *output_file;
 } Tooloptions;
 
-void help_menu() {
-    printf("XorCrypt - XOR Encryption Tool\n\n");
-    printf("Usage:\n");
-    printf("  XorCrypt -e -i <input_file> -k <key> -o <output_file>\n");
-    printf("  XorCrypt -d -i <input_file> -kf <keyfile> -o <output_file>\n\n");
-    
-    printf("Description:\n");
-    printf("  XorCrypt is a simple tool for encrypting and decrypting files using XOR encryption.\n");
-    printf("  It requires a key (provided as a string or a file) and supports both encryption (-e)\n");
-    printf("  and decryption (-d) modes. Use the same key or keyfile for both encryption and decryption.\n\n");
-
-    printf("Options:\n");
-    printf("  -e               Encrypt the input file.\n");
-    printf("  -d               Decrypt the input file.\n");
-    printf("  -i <input_file>  Specify the input file to encrypt or decrypt.\n");
-    printf("  -k <key>         Provide the encryption/decryption key as a string.\n");
-    printf("  -kf <keyfile>    Provide the encryption/decryption key as a file.\n");
-    printf("  -o <output_file> Specify the output file for the encrypted or decrypted data.\n\n");
-
-    printf("Example Usage:\n");
-    printf("  Encrypt using a key string:\n");
-    printf("    XorCrypt -e -i input_file.txt -k \"mysecretkey\" -o encrypted_output.txt\n\n");
-    printf("  Decrypt using a keyfile:\n");
-    printf("    XorCrypt -d -i encrypted_output.txt -kf keyfile.txt -o decrypted_output.txt\n\n");
-		exit(0);
-}
 
 void error_occured(){
 		printf("Error occured please read the help manual again\n");
@@ -99,11 +78,25 @@ int parse_arguments(int argc , char *argv[] , Tooloptions* options){
 	options->output_file = argv[7];
 	return 1;
 }
+
 int main(int argc , char *argv[]){
 	Tooloptions options;
 	if ( parse_arguments(argc , argv , &options) != 1) {
 		error_occured();
 	}
-	print_options(&options);
+	//print_options(&options);  //testing
+	if( crypt(&options) ) {
+		printf("done...success");
+	};
 	return 0;
+}
+
+int crypt(Tooloptions *options){
+
+	if(options->operation == 1){
+		return encrypt(); // encrypt returns 1 if operation is success else returns error;
+	}else{
+		return decrypt(); // same as encrypt;
+	}
+
 }
